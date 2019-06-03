ba1579d7dd99faef1a8fa28f6b1eb4bb5f5e2244
Rhoads <David.Rhoads@carrierenterprise.com>
2019-06-03 16:33:39 -0400

    first commit

---
+++ force-app/main/default/lwc/recordIdTest/recordIdTest.js
@@ -0,0 +1,8 @@
+import { LightningElement,api,wire } from 'lwc';
+import getContacts from '@salesforce/apex/ContactData.getContacts';
+
+export default class RecordIdExample extends LightningElement {
+     @api recordId;
+    @wire(getContacts, {accId:'$recordId' })
+      contacts;
+}
